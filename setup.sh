6cc2253667675bf32f84e05fb4e2bbdf493c1e6f
tsuyoki2000 <84623270+tsuyoki2000@users.noreply.github.com>
2023-08-25 22:03:17 +0900

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -1,1 +1,11 @@
+# /etc/environment no hennsyuu
+ETC_ENV="/etc/environment"
+sudo sh -c "echo EDITOR=nvim >> $ETC_ENV"
+sudo sh -c "echo WLR_NO_HARDWARE_CURSORS=1 >> $ETC_ENV"
+sudo sh -c "echo WLR_RENDERER_ALLOW_SOFTWARE=1 >> $ETC_ENV"
 
+# Base
+sudo pacman -Sy hyprland foot otf-ipafont wofi firefox-i18n-ja --needed --noconfirm
+
+# Config
+cp config/hypr/hyprland.conf $HOME/.config/hypr/
