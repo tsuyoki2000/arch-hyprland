80fd653376cc0ea6c09e51a062d20a40d5deb3f4
tsuyoki2000 <84623270+tsuyoki2000@users.noreply.github.com>
2023-08-25 23:31:08 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,8 +10,8 @@ sudo sh -c "echo EDITOR=nvim >> $ETC_ENV"
 sudo sh -c "echo WLR_NO_HARDWARE_CURSORS=1 >> $ETC_ENV"
 sudo sh -c "echo WLR_RENDERER_ALLOW_SOFTWARE=1 >> $ETC_ENV"
 
-# パッケージのインストール
-sudo pacman -Sy hyprland foot otf-ipafont wofi neovim firefox-i18n-ja --needed --noconfirm
+# 基本パッケージのインストール
+sudo pacman -Sy hyprland foot wofi otf-ipafont --needed --noconfirm
 
 # Hyprland の設定ファイルをコピー
 mkdir -p $HOME/.config/hypr/
@@ -20,6 +20,22 @@ cp config/hypr/hyprland.conf $HOME/.config/hypr/
 # foot の設定ファイルをコピー
 mkdir -p $HOME/.config/foot/
 cp config/foot/foot.ini $HOME/.config/foot/
+
+# 追加パッケージインストール
+sudo pacman -Sy neovim firefox-i18n-ja --needed --noconfirm
+
+# rangerインストール
+sudo pacman -Sy ranger highlight sxiv --needed --noconfirm
+# ranger設定ファイルの作成
+ranger --copy-config all
+
+# ~/.config/ranger/rc.conf の編集
+# 枠線の表示
+sed -i "s/set draw_borders none/set draw_borders both/g" $HOME/.config/ranger/rc.conf
+# 画像表示を「true」にする
+sed -i "s/set preview_images false/set preview_images true/g" $HOME/.config/ranger/rc.conf
+# 画像の表示方法に「ueberzug」を指定
+#sed -i "s/set preview_images_method w3m/set preview_images_method ueberzug/g" $HOME/.config/ranger/rc.conf
 
 # 再起動
 reboot
