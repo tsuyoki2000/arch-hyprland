58aa16d2d7b710edfa6ea5de2b06728c2d920240
tsuyoki2000 <tsuyoki2000@gmail.com>
2024-05-09 19:34:15 +0900

    update hyprland-minimal.sh

--- install-script/hyprland-minimal.sh
+++ install-script/hyprland-minimal.sh
@@ -8,5 +8,5 @@
 sudo pacman -Sy hyprland foot otf-ipafont wofi --needed --noconfirm
 
 # hyprland.conf の編集
-HYPR_CONF="$HOME/.config/hypr/hyprland.conf"
+HYPR_CONF=$HOME/.config/hypr/hyprland.conf
 sed -i "s/$terminal = kitty/$terminal = foot/g" HYPR_CONF
