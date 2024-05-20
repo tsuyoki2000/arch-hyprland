44223d628a48c5df00fc9cdb5a7627992edb8d98
tsuyoki2000 <tsuyoki2000@gmail.com>
2024-05-20 15:59:02 +0900

    update font.sh

--- .install/font.sh
+++ .install/font.sh
@@ -11,6 +11,6 @@ yay -S ttf-hackgen --needed --noconfirm
 # foot.ini の編集
 sed -i "s/font=monospace:/font=HackGen Console NF:/g" $HOME/.config/foot/foot.ini
 
-# HackGenフォントに入れ替えたので、ttf-dejavu-nerd を削除
-sudo pacman -Rs ttf-dejavu-nerd --noconfirm
+# HackGenフォントに入れ替えたので、ttf-dejavu-nerd と otf-ipafont を削除
+sudo pacman -Rs ttf-dejavu-nerd otf-ipafont --noconfirm
 
