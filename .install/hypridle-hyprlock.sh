4c0cb8d179d3e003f0d6ff65efaabd36cacb8767
tsuyoki2000 <tsuyoki2000@gmail.com>
2024-05-20 21:32:09 +0900

    hypridle.conf 修正

--- .install/hypridle-hyprlock.sh
+++ .install/hypridle-hyprlock.sh
@@ -7,9 +7,4 @@
 # パッケージのインストール
 sudo pacman -Sy hypridle hyprlock --needed --noconfirm
 
-# 設定ファイルのコピー
-HYPR_DIR=$HOME/.config/hypr/
-mkdir -p $HYPR_DIR
-cp ../dotfiles/hypr/hypridle.conf $HYPR_DIR
-
 # hyprland.conf に「exec-once = hypridle」を追加
--- setup.sh
+++ setup.sh
@@ -61,6 +61,7 @@ bash .install/neovim-nvchad.sh
 # hypridle と hyprlock のインストール
 green "Installing hypridle and hyprlock..."
 bash .installl/hypridle-hyprlock.sh
+cp dotfiles/hypr/hypridle.conf $HOME/.config/hypr/
 
 
 # 再起動
