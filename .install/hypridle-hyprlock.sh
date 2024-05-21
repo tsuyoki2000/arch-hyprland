c7212a3e2a308b87f1fbc9ff124c22effb0b5ae0
tsuyoki2000 <tsuyoki2000@gmail.com>
2024-05-21 09:28:49 +0900

    add hyprlock.conf

--- .install/hypridle-hyprlock.sh
+++ .install/hypridle-hyprlock.sh
@@ -7,4 +7,6 @@
 # パッケージのインストール
 sudo pacman -Sy hypridle hyprlock --needed --noconfirm
 
+curl https://raw.githubusercontent.com/tsuyoki2000/arch-hyprland/main/dotfiles/hypr/hypridle.conf -o $HOME/.config/hypr/hypridle.conf
+
 # hyprland.conf に「exec-once = hypridle」を追加
