136f1b673e12f0cbf08ac59ddbb06a9219329454
tsuyoki2000 <tsuyoki2000@gmail.com>
2024-05-27 21:38:14 +0900

    fcitx5-system.sh を修正

--- install-script/fcitx5-system.sh
+++ install-script/fcitx5-system.sh
@@ -1,31 +1,30 @@
 #!/bin/bash
 
-########################################################
-# Fcitx5-mozc をインストールするためのシェルスクリプト #
-########################################################
-#ETC_ENV="/etc/environment"
+#########################
+# fcitx5 のインストール #
+#########################
 
 # パッケージのインストール
 # fcitx5-im（fcitx5 パッケージ一式）
 # fcitx5-mozc（fcitx5 用の mozc）
 sudo pacman -Sy fcitx5-im fcitx5-mozc --needed --noconfirm
 
-# /etc/environment にお決まりの呪文を追加
-# 「=fcitx5」と書かなくても「=fcitx」で動作する
-#sudo sh -c "echo GTK_IM_MODULE=fcitx >> $ETC_ENV"
-#sudo sh -c "echo QT_IM_MODULE=fcitx >> $ETC_ENV"
-#sudo sh -c "echo XMODIFIERS=@im=fcitx >> $ETC_ENV"
-
 # 自動起動を hyprland.conf に追加
 echo "exec-once = fcitx5" >> $HOME/.config/hypr/hyprland.conf
 
-# # インライン入力に対応するために、~/.config/fcitx5/conf/xim.conf を追加
-# mkdir -p $HOME/.config/fcitx5/conf
-# echo "# XIM で On The Spot スタイルを使う（再起動が必要）
-# UseOnTheSpot=True
-# " >> $HOME/.config/fcitx5/conf/xim.conf
+# 再ログイン
+echo "Re:Login"
 
-# /etc/environment を編集しているので、設定を有効にするために再起動が必要
-# （/etc/environment は OS起動時に読み込まれる）
-echo "Type reboot"
-#reboot
+
+
+# お決まりの呪文（↓）は Hyprland に必要ない
+# export GTK_IM_MODULE=fcitx
+# export QT_IM_MODULE=fcitx
+# export XMODIFIERS=@im=fcitx
+
+# インライン入力ができない時は…
+# 1. fcitx5 の設定を開き「アドオン」タブを選択
+# 2. 「X Input Method フロントエンド」の設定ボタン
+# 3. 「XIM で On The Spot スタイルを使う（再起動が必要）」にチェックを入れ、OKボタン
+# 4. fcitx の設定画面で「閉じる」ボタン
+# 5. fcitx を再起動
