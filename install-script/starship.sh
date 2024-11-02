572fa5ab59496df8e3e529c7e913445ff8a900e9
tsuyoki2000 <tsuyoki2000@gmail.com>
2024-11-02 23:56:52 +0900

    デザイン変更コマンドを追加

--- install-script/starship.sh
+++ install-script/starship.sh
@@ -11,6 +11,9 @@ sudo pacman -Sy starship --needed --noconfirm
 # shell が fish の場合
 echo "starship init fish | source" >> $HOME/.config/fish/config.fish
 
-# shell が bash の場合
+# shell が bash の場合（未確認）
 #echo 'eval "$(starship init bash)"' >> $HOME/.config/fish/config.fish
+
+# デザインを変更（gruvbox-rainbow の場合）
+starship preset gruvbox-rainbow -o ~/.config/starship.toml
 
