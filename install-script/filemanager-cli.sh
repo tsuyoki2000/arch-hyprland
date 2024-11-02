5d963511699f8a4207b8e99f5c7a9fdb39494f9f
tsuyoki2000 <84623270+tsuyoki2000@users.noreply.github.com>
2024-11-02 10:19:06 +0900

    Update filemanager-cli.sh

--- install-script/filemanager-cli.sh
+++ install-script/filemanager-cli.sh
@@ -40,3 +40,20 @@ curl https://raw.githubusercontent.com/sxyazi/yazi/main/yazi-config/preset/theme
 #   - ターミナルに表示する事も可能だが、ドット絵みたいになる。
 
 
+###########################################################################
+# ranger（CLIファイルマネージャー）をインストールするためのシェルスクリプト #
+###########################################################################
+
+# パッケージのインストール
+#sudo pacman -Sy ranger highlight sxiv --needed --noconfirm
+
+# ranger設定ファイルの作成
+#ranger --copy-config all
+
+# ~/.config/ranger/rc.conf の編集
+# 枠線の表示
+#sed -i "s/set draw_borders none/set draw_borders both/g" $HOME/.config/ranger/rc.conf
+# 画像表示を「true」にする
+#sed -i "s/set preview_images false/set preview_images true/g" $HOME/.config/ranger/rc.conf
+# 画像の表示方法に「ueberzug」を指定
+#sed -i "s/set preview_images_method w3m/set preview_images_method ueberzug/g" $HOME/.config/ranger/rc.conf
