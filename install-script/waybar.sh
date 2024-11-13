a06ce51a97a159155e15d8984c37d0821412bfae
tsuyoki2000 <tsuyoki2000@gmail.com>
2024-11-13 13:49:34 +0900

    shutdown コマンドを poweroff コマンドに変更

--- install-script/waybar.sh
+++ install-script/waybar.sh
@@ -11,6 +11,8 @@ sudo pacman -Sy waybar ttf-font-awesome --needed --noconfirm
 mkdir -p $HOME/.config/waybar/
 cp /etc/xdg/waybar/* $HOME/.config/waybar/
 curl https://raw.githubusercontent.com/Alexays/Waybar/refs/heads/master/resources/custom_modules/power_menu.xml -o $HOME/.config/waybar/power_menu.xml
+# （操作メッセージを無効化）
+sed -i 's/shutdown",/poweroff",/g' $HOME/.config/waybar/config.jsonc
 
 # 自動起動を hyprland.conf に追加
 echo "exec-once = waybar" >> $HOME/.config/hypr/hyprland.conf
