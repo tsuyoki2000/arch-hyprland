a94f3cbb94050731d8e13aa900490a1ec5fa07c6
tsuyoki2000 <84623270+tsuyoki2000@users.noreply.github.com>
2024-11-13 10:55:47 +0900

    Update waybar.sh
    power_menu.xml を github から ダウンロードするコードを追加

--- install-script/waybar.sh
+++ install-script/waybar.sh
@@ -10,6 +10,7 @@ sudo pacman -Sy waybar ttf-font-awesome --needed --noconfirm
 # 設定ファイルのコピー
 mkdir -p $HOME/.config/waybar/
 cp /etc/xdg/waybar/* $HOME/.config/waybar/
+curl https://raw.githubusercontent.com/Alexays/Waybar/refs/heads/master/resources/custom_modules/power_menu.xml -o $HOME/.config/waybar/power_menu.xml
 
 # 自動起動を hyprland.conf に追加
 echo "exec-once = waybar" >> $HOME/.config/hypr/hyprland.conf
