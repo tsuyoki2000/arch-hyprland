1b4c66c538dfb33e7e5cec183d1fcac97d508782
tsuyoki2000 <84623270+tsuyoki2000@users.noreply.github.com>
2025-05-29 18:42:49 +0900

    Update hypridle-hyprlock.sh

--- install-script/hypridle-hyprlock.sh
+++ install-script/hypridle-hyprlock.sh
@@ -8,8 +8,11 @@
 sudo pacman -Sy hypridle hyprlock --needed --noconfirm
 
 # 設定ファイルをコピー
-curl https://raw.githubusercontent.com/tsuyoki2000/arch-hyprland/main/dotfiles/hypr/hypridle.conf -o $HOME/.config/hypr/hypridle.conf
-curl https://raw.githubusercontent.com/tsuyoki2000/arch-hyprland/main/dotfiles/hypr/hyprlock.conf -o $HOME/.config/hypr/hyprlock.conf
+cp /usr/share/hypr/hypridle.conf $HOME/.config/hypr/
+cp /usr/share/hypr/hyprlock.conf $HOME/.config/hypr/
+#curl https://raw.githubusercontent.com/tsuyoki2000/arch-hyprland/main/dotfiles/hypr/hypridle.conf -o $HOME/.config/hypr/hypridle.conf
+#curl https://raw.githubusercontent.com/tsuyoki2000/arch-hyprland/main/dotfiles/hypr/hyprlock.conf -o $HOME/.config/hypr/hyprlock.conf
 
 # 自動起動を hyprland.conf に追加
-echo "exec-once = hypridle" >> $HOME/.config/hypr/hyprland.conf
+#echo "exec-once = hypridle" >> $HOME/.config/hypr/hyprland.conf
+sed -i "s/# exec-once = $terminal/exec-once = hypridle\n# exec-once = $terminal/g" $HOME/.config/hypr/hyprland.conf
