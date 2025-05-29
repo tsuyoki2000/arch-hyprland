b82c78b181f6efcee8868c963b49d3f5cf781196
tsuyoki2000 <84623270+tsuyoki2000@users.noreply.github.com>
2025-05-29 22:09:22 +0900

    Add files via upload

---
+++ install-script/wlogout.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+####################################################
+# wlogout をインストールするためのシェルスクリプト #
+####################################################
+
+# パッケージのインストール
+yay -Sy wlogout --needed --noconfirm
+
+# 設定ファイルをコピー
+curl https://raw.githubusercontent.com/ArtsyMacaw/wlogout/refs/heads/master/layout -o $HOME/.config/wlogout/layout
+curl https://raw.githubusercontent.com/ArtsyMacaw/wlogout/refs/heads/master/style.css -o $HOME/.config/wlogout/style.css
